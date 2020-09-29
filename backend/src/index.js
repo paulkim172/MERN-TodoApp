36bb88d7d2f87593b703e393cb49303ba1438b16
paulkim172 <55893592+paulkim172@users.noreply.github.com>
2020-09-29 15:12:50 -0400

    connected to MongoDB Atlas

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,11 @@
-const express = require('express')
+const {URI} = require('./credentials.js');
+const mongoose = require('mongoose');
+const express = require('express');
+
 const app = express()
 const port = 3000
+
+mongoose.connect(URI, {useNewUrlParser: true, useUnifiedTopology: true});
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -9,3 +14,10 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+    console.log('Connected to MongoDB!')
+});
+
